bd5e317f7ead66efe03e26134e48a4840c3a3600
BozenaPalicCeric <bozena.palic@gmail.com>
2020-02-23 22:28:04 +0100

    Update godisnjiodmor.sql

--- godisnjiodmor.sql
+++ godisnjiodmor.sql
@@ -6,7 +6,7 @@ create table zaposlenik (
     sifra int not null primary key auto_increment,
     ime varchar(50) not null,
     prezime varchar(50) not null,
-    oib char (11),
+    oib varchar (11),
     email varchar(50) not null,
     datumzaposlenja date not null,
     nadredeni int
@@ -40,7 +40,7 @@ insert into zaposlenik (ime,prezime,oib,email,datumzaposlenja) values
 ('Jelena', 'Mikić', '93103467263', 'jmikic@gmail.com', '2019-02-01'),
 ('Elena', 'Horvat', '90505574665', 'elenah@gmail.com', '2019-02-15'),
 ('Ivan', 'Knežević', '29058715389', 'ivan.knez@gmail.com', '2019-03-01'),
-('Blaž', 'Pavlović', '42565827138' 'blaz.pavlovic@gmail.com', '2019-03-20'),
+('Blaž', 'Pavlović', '42565827138', 'blaz.pavlovic@gmail.com', '2019-03-20'),
 ('Maja', 'Novak', '87534789882', 'majan@gmail.com', '2019-04-01'),
 ('Igor', 'Tomas', '40602896341', 'tigor@gmail.com', '2019-05-01');
 
