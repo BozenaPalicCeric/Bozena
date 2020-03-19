9d72d3cbd0c3886a3378af5ffedad16ba00247f6
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-20 00:11:30 +0100

    Create Start.java

---
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/Start.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.godisnjiodmor_app;
+
+import hr.godisnjiodmor_app.util.HibernateUtil;
+
+/**
+ *
+ * @author Bozena
+ */
+public class Start {
+    public Start () {
+    
+        HibernateUtil.getSessionFactory().openSession();
+}
+    public static void main(String[] args) {
+        new Start();
+    }
+}
