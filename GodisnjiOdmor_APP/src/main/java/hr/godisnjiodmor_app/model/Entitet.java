95cdc94111d0fb84198a05d3a8a58d119d6daf31
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-20 00:16:54 +0100

    Update Entitet.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/model/Entitet.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/model/Entitet.java
@@ -18,7 +18,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class Entitet {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer sifra;
 
     public Integer getSifra() {
