d299b28234b379fea4b1d6c8a0852e3e6ce660a3
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-23 18:58:42 +0100

    Update Obrada.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/Obrada.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/Obrada.java
@@ -7,6 +7,7 @@ package hr.godisnjiodmor_app.controller;
 
 import hr.godisnjiodmor_app.util.GodisnjiException;
 import hr.godisnjiodmor_app.util.HibernateUtil;
+import java.util.List;
 import org.hibernate.Session;
 
 /**
@@ -19,20 +20,46 @@ public abstract class Obrada<T> {
     protected abstract void kontrolaCreate() throws GodisnjiException;
     protected abstract void kontrolaUpdate() throws GodisnjiException;
     protected abstract void kontrolaDelete() throws GodisnjiException;
+    public abstract List<T> getPodaci();
+    protected abstract void nakonSpremanja() throws GodisnjiException;
     
-    public Obrada(T entitet){
+     public Obrada(T entitet){
+        this();
         this.entitet=entitet;
+    }
+
+    public Obrada() {
         session = HibernateUtil.getSessionFactory().openSession();
     }
     
+    
+    
     public T create() throws GodisnjiException{
         kontrolaCreate();
-        return save();
+        save();
+        nakonSpremanja();
+        return entitet;
+    }
+    
+    public void createAll(List<T> lista) throws GodisnjiException{
+    
+        session.beginTransaction();
+        for(T sl : lista){
+            this.entitet=sl;
+            kontrolaCreate();
+            session.save(sl);
+            nakonSpremanja();
+        }
+        session.getTransaction().commit();
+        
+        
     }
     
     public T update() throws GodisnjiException{
         kontrolaUpdate();
-        return save();
+        save();
+        nakonSpremanja();
+        return entitet;
     }
     
     public boolean delete() throws GodisnjiException{
@@ -43,11 +70,9 @@ public abstract class Obrada<T> {
         return true;
     }
     
-    private T save(){
+    private void save(){
         session.beginTransaction();
         session.save(entitet);
         session.getTransaction().commit();
-        return entitet;
     }
-      
 }
