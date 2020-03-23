b7ec4fe7ebddf6886b1b986fb55ed8729fb19f68
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-23 18:58:52 +0100

    Update ObradaZaposlenik.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/ObradaZaposlenik.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/ObradaZaposlenik.java
@@ -8,25 +8,30 @@ package hr.godisnjiodmor_app.controller;
 import hr.godisnjiodmor_app.model.Zaposlenik;
 import hr.godisnjiodmor_app.util.GodisnjiException;
 import hr.godisnjiodmor_app.util.Pomocno;
+import java.util.List;
 
 /**
  *
  * @author Bozena
  */
-public class ObradaZaposlenik <T extends Zaposlenik> extends Obrada<T>{
+public class ObradaZaposlenik extends Obrada<Zaposlenik>{
 
-    public ObradaZaposlenik(T entitet) {
+    public ObradaZaposlenik(Zaposlenik entitet) {
         super(entitet);
+    }
+
+    public ObradaZaposlenik() {
     }
 
     @Override
     protected void kontrolaCreate() throws GodisnjiException {
         kontrolaOib();
+        kontrolaEmail();
     }
 
     @Override
     protected void kontrolaUpdate() throws GodisnjiException {
-       
+        
     }
 
     @Override
@@ -34,10 +39,25 @@ public class ObradaZaposlenik <T extends Zaposlenik> extends Obrada<T>{
         
     }
 
-    private void kontrolaOib() throws GodisnjiException{
-        if(!Pomocno.isOibValjan(entitet.getOib())) {
-            throw new GodisnjiException("OIB nije valjan");
-        }
+    @Override
+    public List<Zaposlenik> getPodaci() {
+        return session.createQuery("from Zaposlenik").list();
     }
-    
+
+    @Override
+    protected void nakonSpremanja() throws GodisnjiException {
+       
+    }
+
+    private void kontrolaOib() throws GodisnjiException{
+        if(!Pomocno.isOibValjan(entitet.getOib())){
+            throw new GodisnjiException("OIB nije valjan");
+    }
+
+    }
+
+    private void kontrolaEmail() {
+        
+    }
+
 }
