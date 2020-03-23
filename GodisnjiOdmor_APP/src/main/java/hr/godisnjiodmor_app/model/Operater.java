aca35d32bf6a05b3b0166ad88daafab8984bac25
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-23 18:58:56 +0100

    Create Operater.java

---
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/model/Operater.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.godisnjiodmor_app.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Bozena
+ */
+
+@Entity
+public class Operater extends Zaposlenik{
+    private String lozinka;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+    
+    
+}
