1a3d601521090fa9e1169073adad37db2f188052
BozenaPalicCeric <bozena.palic@gmail.com>
2020-03-23 18:58:44 +0100

    Create ObradaEvidencijaGodisnjiOdmor.java

---
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/ObradaEvidencijaGodisnjiOdmor.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.godisnjiodmor_app.controller;
+
+import hr.godisnjiodmor_app.model.EvidencijaGodisnjiOdmor;
+import hr.godisnjiodmor_app.util.GodisnjiException;
+import java.util.List;
+
+/**
+ *
+ * @author Bozena
+ */
+public class ObradaEvidencijaGodisnjiOdmor extends Obrada<EvidencijaGodisnjiOdmor>{
+
+    public ObradaEvidencijaGodisnjiOdmor(EvidencijaGodisnjiOdmor entitet) {
+        super(entitet);
+    }
+
+    public ObradaEvidencijaGodisnjiOdmor() {
+    }
+
+    
+    @Override
+    protected void kontrolaCreate() throws GodisnjiException {
+        
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws GodisnjiException {
+        
+    }
+
+    @Override
+    protected void kontrolaDelete() throws GodisnjiException {
+        
+    }
+
+    @Override
+    public List<EvidencijaGodisnjiOdmor> getPodaci() {
+        return session.createQuery("from EvidencijaGodisnjiOdmor").list();
+    }
+
+    @Override
+    protected void nakonSpremanja() throws GodisnjiException {
+        
+    }
+    
+    
+}
