650a21aa8234955b449e685e86a110363affcf4f
BozenaPalicCeric <bozena.palic@gmail.com>
2020-04-10 14:11:24 +0200

    Update ObradaZaposlenik.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/ObradaZaposlenik.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/ObradaZaposlenik.java
@@ -5,10 +5,12 @@
  */
 package hr.godisnjiodmor_app.controller;
 
+import hr.godisnjiodmor_app.model.GodisnjiOdmor;
 import hr.godisnjiodmor_app.model.Zaposlenik;
 import hr.godisnjiodmor_app.util.GodisnjiException;
 import hr.godisnjiodmor_app.util.Pomocno;
 import java.util.List;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  *
@@ -21,12 +23,34 @@ public class ObradaZaposlenik extends Obrada<Zaposlenik>{
     }
 
     public ObradaZaposlenik() {
+    super();
     }
+    
+    public Zaposlenik autoriziraj(String email, String lozinka){
+        
+        List<Zaposlenik> lista = session.createQuery("from Zaposlenik z "
+                + " where z.email=:email")
+                .setParameter("email", email).list();
+        if(lista==null || lista.isEmpty()){
+            return null;
+        }
+        Zaposlenik z = lista.get(0);
+        if(z==null){
+            return null;
+        }
+        
+        return BCrypt.checkpw(lozinka, z.getLozinka()) ? z : null;
+    }
+    
 
     @Override
     protected void kontrolaCreate() throws GodisnjiException {
         kontrolaOib();
         kontrolaEmail();
+        //kontrolaLozinka();
+        kontrolaIstiOib();
+        
+        
     }
 
     @Override
@@ -36,7 +60,14 @@ public class ObradaZaposlenik extends Obrada<Zaposlenik>{
 
     @Override
     protected void kontrolaDelete() throws GodisnjiException {
-        
+        if(!entitet.getGodisnjiOdmori().isEmpty()){
+          String s="";
+          for (GodisnjiOdmor g : entitet.getGodisnjiOdmori()) {
+              s+=g.getPocetakGodisnjiOdmor() + ", ";
+          }
+          s=s.substring(0,s.length()-2);
+          throw new GodisnjiException("Zaposlenik se ne može obrisati jer na njemu postoje vezani podaci: \n"+s);
+      }
     }
 
     @Override
@@ -59,5 +90,26 @@ public class ObradaZaposlenik extends Obrada<Zaposlenik>{
     private void kontrolaEmail() {
         
     }
+
+    private void kontrolaIstiOib() throws GodisnjiException {
+     Long ukupno = (Long)session
+            .createQuery(" select count(z) from Zaposlenik z "
+                    + " where z.oib=:oib")
+              .setParameter("oib", entitet.getOib())
+              .uniqueResult();
+      if(ukupno>0){
+          throw  new GodisnjiException("U bazi postoji isti OIB, provjerite zaposlenika");
+      }   
+    }
+
+    private void kontrolaLozinka() throws GodisnjiException{
+        if(entitet.getLozinka()==null || entitet.getLozinka().trim().length()==0){
+            throw new GodisnjiException("Obavezno lozinka"); 
+        }
+    }
+
+   
+
+    
 
 }
