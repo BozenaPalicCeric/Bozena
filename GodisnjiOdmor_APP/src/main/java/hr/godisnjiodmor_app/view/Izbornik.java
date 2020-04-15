b53fd7941863d86f5baf1084205d2ab03174bbc6
BozenaPalicCeric <bozena.palic@gmail.com>
2020-04-15 21:20:20 +0200

    Update Izbornik.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/view/Izbornik.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/view/Izbornik.java
@@ -23,11 +23,12 @@ public class Izbornik extends javax.swing.JFrame {
         initComponents();
         setTitle(Pomocno.getNazivAplikacije());
         jmAplikacija.setText(Pomocno.getNazivAplikacije());
-        
+
         Vrijeme v = new Vrijeme();
         v.start();
     }
-    private class Vrijeme extends Thread{
+
+    private class Vrijeme extends Thread {
 
         SimpleDateFormat df = new SimpleDateFormat("dd. MM. yyyy. HH:mm:ss");
 
@@ -37,13 +38,12 @@ public class Izbornik extends javax.swing.JFrame {
                 lblVrijeme.setText(df.format(new Date()));
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
-                
+
             }
             run();
         }
-        }
-        
-        
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -62,6 +62,7 @@ public class Izbornik extends javax.swing.JFrame {
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -92,7 +93,7 @@ public class Izbornik extends javax.swing.JFrame {
         });
         jMenu2.add(jMenuItem2);
 
-        jMenuItem3.setText("Godišnji odmor");
+        jMenuItem3.setText("Evidencija godišnjeg odmora");
         jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem3ActionPerformed(evt);
@@ -100,13 +101,21 @@ public class Izbornik extends javax.swing.JFrame {
         });
         jMenu2.add(jMenuItem3);
 
-        jMenuItem4.setText("Evidencija godišnjeg odmora");
+        jMenuItem4.setText("Godišnji odmor");
         jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem4ActionPerformed(evt);
             }
         });
         jMenu2.add(jMenuItem4);
+
+        jMenuItem5.setText("Evidencija2");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem5);
 
         jMenuBar1.add(jMenu2);
 
@@ -141,17 +150,20 @@ public class Izbornik extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        new ViewGodisnjiOdmor().setVisible(true);
+        new ViewEvidencijaGodisnjiOdmor().setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        new ViewEvidencijaGodisnjiOdmor().setVisible(true);
+        new ViewGodisnjiOdmor().setVisible(true);
     }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        new Evidencija2().setVisible(true);
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu2;
@@ -160,8 +172,9 @@ public class Izbornik extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JToolBar jToolBar1;
     private javax.swing.JMenu jmAplikacija;
     private javax.swing.JLabel lblVrijeme;
     // End of variables declaration//GEN-END:variables
-    }
+}
