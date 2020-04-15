495c4ebc7c51839e7e110a90fa8e4706a33287da
BozenaPalicCeric <bozena.palic@gmail.com>
2020-04-15 21:19:38 +0200

    Update Obrada.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/Obrada.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/controller/Obrada.java
@@ -15,23 +15,27 @@ import org.hibernate.Session;
  * @author Bozena
  */
 public abstract class Obrada<T> {
-    
+
     protected T entitet;
     protected Session session;
+
     protected abstract void kontrolaCreate() throws GodisnjiException;
+
     protected abstract void kontrolaUpdate() throws GodisnjiException;
+
     protected abstract void kontrolaDelete() throws GodisnjiException;
+
     public abstract List<T> getPodaci();
+
     protected abstract void nakonSpremanja() throws GodisnjiException;
-    
-    
+
     public Obrada() {
         session = HibernateUtil.getSessionFactory().openSession();
     }
-    
-     public Obrada(T entitet){
+
+    public Obrada(T entitet) {
         this();
-        this.entitet=entitet;
+        this.entitet = entitet;
     }
 
     public T getEntitet() {
@@ -42,51 +46,43 @@ public abstract class Obrada<T> {
         this.entitet = entitet;
     }
 
-    
-
-    
-
-    
-    
-    
-    public T create() throws GodisnjiException{
+    public T create() throws GodisnjiException {
         System.out.println(entitet);
         kontrolaCreate();
         save();
         nakonSpremanja();
         return entitet;
     }
-    
-    public void createAll(List<T> lista) throws GodisnjiException{
-    
+
+    public void createAll(List<T> lista) throws GodisnjiException {
+
         session.beginTransaction();
-        for(T sl : lista){
-            this.entitet=sl;
+        for (T sl : lista) {
+            this.entitet = sl;
             kontrolaCreate();
             session.save(sl);
             nakonSpremanja();
         }
         session.getTransaction().commit();
-        
-        
+
     }
-    
-    public T update() throws GodisnjiException{
+
+    public T update() throws GodisnjiException {
         kontrolaUpdate();
         save();
         nakonSpremanja();
         return entitet;
     }
-    
-    public boolean delete() throws GodisnjiException{
+
+    public boolean delete() throws GodisnjiException {
         kontrolaDelete();
         session.beginTransaction();
         session.delete(entitet);
         session.getTransaction().commit();
         return true;
     }
-    
-    private void save(){
+
+    private void save() {
         session.beginTransaction();
         session.save(entitet);
         session.getTransaction().commit();
