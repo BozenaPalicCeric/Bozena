798f097eda77735918f69c1c9ba97edd37023081
BozenaPalicCeric <bozena.palic@gmail.com>
2020-04-15 21:20:05 +0200

    Update Pomocno.java

--- GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/util/Pomocno.java
+++ GodisnjiOdmor_APP/src/main/java/hr/godisnjiodmor_app/util/Pomocno.java
@@ -34,65 +34,61 @@ import org.mindrot.jbcrypt.BCrypt;
  *
  * @author Bozena
  */
-
-
 public class Pomocno {
-    
+
     public static Zaposlenik LOGIRAN;
-    
-     private final static DecimalFormat df = df();
-    
-      
-    public static String getNazivAplikacije(){
+
+    private final static DecimalFormat df = df();
+
+    public static String getNazivAplikacije() {
         return "Godišnji odmor APP";
     }
-    
-    
-    public static String getDatum (Date d){
-        SimpleDateFormat formatter= new SimpleDateFormat("dd.MM.yyyy");
+
+    public static String getDatum(Date d) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
         return formatter.format(d);
     }
+
     public static Date getDatumIzStringa(String s) {
         try {
-            SimpleDateFormat formatter= new SimpleDateFormat("dd.MM.yyyy");
+            SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
             return formatter.parse(s);
         } catch (Exception e) {
             return null;
         }
     }
 
-    public static String getFormatCijelogBroja(long i){
-        
+    public static String getFormatCijelogBroja(long i) {
+
         DecimalFormat dfCijeliBroj = new DecimalFormat("#");
         return dfCijeliBroj.format(i);
     }
-    
-    public static int getCijeliBrojIzStringa(String s){
-         try {
+
+    public static int getCijeliBrojIzStringa(String s) {
+        try {
             return Integer.parseInt(s);
         } catch (Exception e) {
             return 0;
         }
     }
-   
-    private static DecimalFormat df(){
+
+    private static DecimalFormat df() {
         NumberFormat nf = NumberFormat.
-                getNumberInstance(new Locale("hr","HR"));
-         DecimalFormat dfl = (DecimalFormat) nf;
-         dfl.applyPattern("#,###.00");
-    return dfl;
+                getNumberInstance(new Locale("hr", "HR"));
+        DecimalFormat dfl = (DecimalFormat) nf;
+        dfl.applyPattern("#,###.00");
+        return dfl;
     }
-    
+
     public static boolean isOibValjan(String oib) {
-        
-        if(oib==null){
+
+        if (oib == null) {
             return false;
         }
-            
-            
 
-        if (oib.length() != 11)
+        if (oib.length() != 11) {
             return false;
+        }
 
         try {
             Long.parseLong(oib);
@@ -102,189 +98,184 @@ public class Pomocno {
 
         int a = 10;
         for (int i = 0; i < 10; i++) {
-            a = a + Integer.parseInt(oib.substring(i, i+1));
+            a = a + Integer.parseInt(oib.substring(i, i + 1));
             a = a % 10;
-            if (a == 0)
+            if (a == 0) {
                 a = 10;
+            }
             a *= 2;
             a = a % 11;
         }
         int kontrolni = 11 - a;
-        if (kontrolni == 10)
+        if (kontrolni == 10) {
             kontrolni = 0;
+        }
 
         return kontrolni == Integer.parseInt(oib.substring(10));
     }
-    
+
     public static String getGenOIB() {
-        
-        try{
-        URL u = new URL("http://oib.itcentrala.com/oib-generator/");
-        InputStream in = u.openStream();
-            String s = new String(in.readAllBytes(), 
+
+        try {
+            URL u = new URL("http://oib.itcentrala.com/oib-generator/");
+            InputStream in = u.openStream();
+            String s = new String(in.readAllBytes(),
                     StandardCharsets.UTF_8);
-           // System.out.println(s);
-            int pocIndex=s.indexOf("<div class=\"oib\"><span>HR</span>")+32;
-            return s.substring(pocIndex,pocIndex+11);
-        }catch(Exception e){
+            // System.out.println(s);
+            int pocIndex = s.indexOf("<div class=\"oib\"><span>HR</span>") + 32;
+            return s.substring(pocIndex, pocIndex + 11);
+        } catch (Exception e) {
             return "";
         }
-        
+
     }
-   
 
     public static void pocetniInsert() {
-        
-        Zaposlenik zaposlenik= new Zaposlenik();
+
+        Zaposlenik zaposlenik = new Zaposlenik();
         zaposlenik.setEmail("bozena.palic@gmail.com");
         zaposlenik.setIme("Božena");
         zaposlenik.setPrezime("Palić Cerić");
         zaposlenik.setOib("18298417901");
         zaposlenik.setLozinka(BCrypt.hashpw("b", BCrypt.gensalt()));
         zaposlenik.setBrojDanaGoPremaUgovoruORadu(20);
-        Calendar cb= Calendar.getInstance();
+        Calendar cb = Calendar.getInstance();
         cb.set(Calendar.YEAR, 2019);
         cb.set(Calendar.MONTH, 0);
         cb.set(Calendar.DAY_OF_MONTH, 1);
         zaposlenik.setDatumZaposlenja(cb.getTime());
-        
-       
+        zaposlenik.setNadredeni(zaposlenik);
+
         ObradaZaposlenik obradaZaposlenik = new ObradaZaposlenik(zaposlenik);
         try {
-            zaposlenik=obradaZaposlenik.create();
+            zaposlenik = obradaZaposlenik.create();
         } catch (GodisnjiException ex) {
             System.out.println(ex.getPoruka());
         }
-        
-        GodisnjiOdmor godisnjiOdmorb= new GodisnjiOdmor();
+
+        GodisnjiOdmor godisnjiOdmorb = new GodisnjiOdmor();
         godisnjiOdmorb.setZaposlenik(zaposlenik);
-        Calendar cab= Calendar.getInstance();
+        Calendar cab = Calendar.getInstance();
         cab.set(Calendar.YEAR, 2020);
         cab.set(Calendar.MONTH, 1);
         cab.set(Calendar.DAY_OF_MONTH, 3);
         godisnjiOdmorb.setPocetakGodisnjiOdmor(cab.getTime());
-        Calendar calb= Calendar.getInstance();
+        Calendar calb = Calendar.getInstance();
         calb.set(Calendar.YEAR, 2020);
         calb.set(Calendar.MONTH, 1);
         calb.set(Calendar.DAY_OF_MONTH, 14);
         godisnjiOdmorb.setKrajGodisnjiOdmor(calb.getTime());
         godisnjiOdmorb.setKoristenBrojDanaGo(10);
-        
-        
-         ObradaGodisnjiOdmor obradaGodisnjiOdmorb= new ObradaGodisnjiOdmor(godisnjiOdmorb);
+        godisnjiOdmorb.setGodina(2020);
+        godisnjiOdmorb.setOdobrenjeNadredeni(true);
+
+        ObradaGodisnjiOdmor obradaGodisnjiOdmorb = new ObradaGodisnjiOdmor(godisnjiOdmorb);
         try {
             obradaGodisnjiOdmorb.create();
         } catch (GodisnjiException ex) {
             System.out.println(ex.getPoruka());
         }
-        
-        Zaposlenik  z= new Zaposlenik();
+
+        Zaposlenik z = new Zaposlenik();
         z.setIme("Pero");
         z.setPrezime("Petrović");
         z.setOib("95647933683");
         z.setEmail("p.petrovic@gmail.com");
-        Calendar c= Calendar.getInstance();
+        Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR, 2019);
         c.set(Calendar.MONTH, 0);
         c.set(Calendar.DAY_OF_MONTH, 1);
         z.setDatumZaposlenja(c.getTime());
         z.setBrojDanaGoPremaUgovoruORadu(20);
-        //z.setNadredeni(1);
-        
-        ObradaZaposlenik oz= new ObradaZaposlenik(z);
+        z.setNadredeni(zaposlenik);
+
+        ObradaZaposlenik oz = new ObradaZaposlenik(z);
         try {
-            z=oz.create();
+            z = oz.create();
         } catch (GodisnjiException ex) {
-            
+
             System.out.println(ex.getPoruka());
         }
-        
-        GodisnjiOdmor godisnjiOdmor= new GodisnjiOdmor();
+
+        GodisnjiOdmor godisnjiOdmor = new GodisnjiOdmor();
         godisnjiOdmor.setZaposlenik(z);
-        Calendar ca= Calendar.getInstance();
+        Calendar ca = Calendar.getInstance();
         ca.set(Calendar.YEAR, 2020);
         ca.set(Calendar.MONTH, 1);
         ca.set(Calendar.DAY_OF_MONTH, 3);
         godisnjiOdmor.setPocetakGodisnjiOdmor(ca.getTime());
-        Calendar cal= Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, 2020);
         cal.set(Calendar.MONTH, 1);
         cal.set(Calendar.DAY_OF_MONTH, 14);
         godisnjiOdmor.setKrajGodisnjiOdmor(cal.getTime());
         godisnjiOdmor.setKoristenBrojDanaGo(10);
-        
-        
-         ObradaGodisnjiOdmor obradaGodisnjiOdmor= new ObradaGodisnjiOdmor(godisnjiOdmor);
+        godisnjiOdmor.setGodina(2019);
+        godisnjiOdmor.setOdobrenjeNadredeni(true);
+
+        ObradaGodisnjiOdmor obradaGodisnjiOdmor = new ObradaGodisnjiOdmor(godisnjiOdmor);
         try {
             obradaGodisnjiOdmor.create();
         } catch (GodisnjiException ex) {
             System.out.println(ex.getPoruka());
         }
-        
+
         //z.getGodisnjiOdmori().add(godisnjiOdmor);
-        
-        
-     
-        Zaposlenik zaposlenik1=new Zaposlenik();
+        Zaposlenik zaposlenik1 = new Zaposlenik();
         zaposlenik1.setIme("Miki");
         zaposlenik1.setPrezime("Mikić");
         zaposlenik1.setOib("34778528808");
         zaposlenik1.setEmail("miki1@gmail.com");
-        Calendar c1= Calendar.getInstance();
+        Calendar c1 = Calendar.getInstance();
         c1.set(Calendar.YEAR, 2019);
         c1.set(Calendar.MONTH, 0);
         c1.set(Calendar.DAY_OF_MONTH, 1);
         zaposlenik1.setDatumZaposlenja(c1.getTime());
         zaposlenik1.setBrojDanaGoPremaUgovoruORadu(20);
-        //zaposlenik1.setNadredeni(1);
-        
+        zaposlenik1.setNadredeni(zaposlenik);
+
         obradaZaposlenik = new ObradaZaposlenik(zaposlenik1);
         try {
-            zaposlenik1=obradaZaposlenik.create();
+            zaposlenik1 = obradaZaposlenik.create();
         } catch (GodisnjiException ex) {
-             System.out.println(ex.getPoruka());
+            System.out.println(ex.getPoruka());
         }
-        
-        
-        GodisnjiOdmor godisnjiOdmor1= new GodisnjiOdmor();
+
+        GodisnjiOdmor godisnjiOdmor1 = new GodisnjiOdmor();
         godisnjiOdmor1.setZaposlenik(zaposlenik1);
-        Calendar ca1= Calendar.getInstance();
+        Calendar ca1 = Calendar.getInstance();
         ca1.set(Calendar.YEAR, 2020);
         ca1.set(Calendar.MONTH, 2);
         ca1.set(Calendar.DAY_OF_MONTH, 2);
         godisnjiOdmor1.setPocetakGodisnjiOdmor(ca1.getTime());
-        Calendar cal1= Calendar.getInstance();
+        Calendar cal1 = Calendar.getInstance();
         cal1.set(Calendar.YEAR, 2020);
         cal1.set(Calendar.MONTH, 2);
         cal1.set(Calendar.DAY_OF_MONTH, 13);
         godisnjiOdmor1.setKrajGodisnjiOdmor(cal1.getTime());
         godisnjiOdmor1.setKoristenBrojDanaGo(10);
-        
-        ObradaGodisnjiOdmor obradaGodisnjiOdmor1= new ObradaGodisnjiOdmor(godisnjiOdmor1);
+        godisnjiOdmor1.setGodina(2020);
+        godisnjiOdmor1.setOdobrenjeNadredeni(true);
+
+        ObradaGodisnjiOdmor obradaGodisnjiOdmor1 = new ObradaGodisnjiOdmor(godisnjiOdmor1);
         try {
             obradaGodisnjiOdmor1.create();
         } catch (GodisnjiException ex) {
             System.out.println(ex.getPoruka());
         }
-        
-        
+
         //zaposlenik1.getGodisnjiOdmori().add(godisnjiOdmor1);
-        
-        
-        
-        EvidencijaGodisnjiOdmor ego=new EvidencijaGodisnjiOdmor();
-        ego.setGodina(2020);
-        ego.setZaposlenik(z);
-        ego.setPreostaliBrojDanaGodisnjiOdmor(10);
-        
-        ObradaEvidencijaGodisnjiOdmor oego= new ObradaEvidencijaGodisnjiOdmor(ego);
-        
+        EvidencijaGodisnjiOdmor evidencijaGodisnjiOdmor = new EvidencijaGodisnjiOdmor();
+        evidencijaGodisnjiOdmor.setZaposlenik(z);
+        evidencijaGodisnjiOdmor.setPreostaliBrojDanaGodisnjiOdmor(10);
+
+        ObradaEvidencijaGodisnjiOdmor oego = new ObradaEvidencijaGodisnjiOdmor(evidencijaGodisnjiOdmor);
+
         try {
             oego.create();
         } catch (GodisnjiException ex) {
             System.out.println(ex.getPoruka());
         }
     }
-    
 
 }
